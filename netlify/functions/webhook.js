92d609828d4a47e7cd510e78acb26e0e367080dd
Bablu558 <kumarbablu03240@gmail.com>
2024-08-05 12:14:57 +0530

    change for payment

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -2,13 +2,13 @@ const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 const path = require('path');
 
-const secret = process.env.SECRET_KEY;
+const secret = 'mySecretKeyKushwaha1233';
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: 'kushwahabablu40@gmail.com',
+        pass: 'kushwaha'
     }
 });
 
@@ -20,7 +20,7 @@ exports.handler = async (event) => {
             const payment = webhookEvent.resource;
 
             const mailOptions = {
-                from: process.env.EMAIL_USER,
+                from: 'kushwahabablu40@gmail.com',
                 to: payment.payer.email_address,
                 subject: 'Your Purchased Zip File',
                 text: 'Thank you for your purchase! Please find the attached zip file.',
